dbd8fb04c9287302ae2356aa96b937a3d75f5bae
Jamie <goodsonjamie@gmail.com>
2018-03-27 19:24:35 +0100

    Use less instead of css

--- webserver/js/constants.js
+++ webserver/js/constants.js
@@ -1,21 +1,37 @@
-const CharCodes = {
+export const App = {
+	TITLE: 'OCAP',
+	TITLE_FULL: 'Operation Capture And Playback',
+	VERSION: '1.0',
+	AUTHOR: 'MisterGoodson',
+	COMMUNITY_TITLE: '3 Commando Brigade',
+	COMMUNITY_URL: 'www.3commandobrigade.com',
+	BI_THREAD_URL: 'https://forums.bohemia.net/forums/topic/194164-ocap-op-capture-and-playback-aarreplay/',
+	GITHUB_URL: 'https://github.com/mistergoodson/OCAP',
+}
+
+export const CharCode = {
 	SPACE: 32,
 	E: 101,
 	R: 114,
 };
 
-const EntityTypes = {
+export const EntityType = {
 	UNIT: 'unit',
 	VEHICLE: 'vehicle',
-}
+};
 
-let constants = {
-	CharCodes: CharCodes,
-	EntityTypes: EntityTypes,
-	MAPS_PATH: '/static/images/maps',
-	CAPTURES_PATH: '/static/captures',
-	MARKERS_PATH: '/static/images/markers',
-	MAP_META_FILENAME: 'meta.json',
-}
+export const ElementId = {
+};
 
-export {constants};
+export const ClassName = {
+	PAUSE: 'paused',
+	EVENT_TIMELINE_TICK: 'tick',
+	EVENT_DETAILS: 'event-details',
+	EVENT: 'event',
+	REVEAL: 'reveal',
+};
+
+export const CAPTURES_PATH = '/static/captures';
+export const MAP_META_FILENAME = 'meta.json';
+export const MAPS_PATH = '/static/images/maps';
+export const MARKERS_PATH = '/static/images/markers';
